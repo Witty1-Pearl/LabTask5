7078e170be0267a1da50d47049b5d547154ce70b
Prince Nkanu <basyprince@gmail.com>
2020-10-23 02:25:44 +0100

    first commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class demo {
+    public static int mulDisplay(int one, int two) {
+        return one * two;
+    }
+
+    public static int mulDisplay (int one, int two, int three) {
+        return one * two * three;
+    }
+    public static int mulDisplay (int one, int two, int three, int four) {
+        return one * two * three * four;
+    }
+
+
+}
+
+
+public class program {
+    public static void Main() {
+        Console.WriteLine("Multiplication of two numbers: "+demo.mulDisplay(10, 15));
+        Console.WriteLine("Multiplication of three numbers: "+demo.mulDisplay(8, 13, 20));
+        Console.WriteLine("Multiplication of four numbers: "+demo.mulDisplay(3, 7, 10, 7));
+    }
+}
---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    public class Bclass
+    {
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("I am leaning how to code in C-sharp");
+        }
+    
+    }
+
+    public class Dclass : Bclass
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine("Welcome to CodebitsAcademy");
+        }
+    }
+
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Dclass d = new Dclass();
+            d.GetInfo();
+            Bclass b = new Bclass();
+            b.GetInfo();
+            Console.WriteLine("\nPress Enter Key to Exit..");
+
+            Console.ReadLine();
+        }
+    }
+}
